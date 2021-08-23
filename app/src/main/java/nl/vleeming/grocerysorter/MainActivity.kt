02802f84c3d0ee2d3b03cda65544c7c5f798667f
Michiel Vleeming <Michiel.vleeming@nos.nl>
2021-08-23 15:48:25 +0200

    initial commit

---
+++ app/src/main/java/nl/vleeming/grocerysorter/MainActivity.kt
@@ -0,0 +1,39 @@
+package nl.vleeming.grocerysorter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import nl.vleeming.grocerysorter.ui.theme.GrocerySorterTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GrocerySorterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GrocerySorterTheme {
+        Greeting("Android")
+    }
+}
